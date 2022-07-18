385774ba007ae4d4d31b567aa8bcc45f453758f4
tengfeihan <1157123521@qq.com>
2022-07-18 11:46:07 +0800

    自定义宽高

--- src/core.js
+++ src/core.js
@@ -1,10 +1,15 @@
 import { addEvent, isDOM, createElement, getElementById } from "./utils";
 import { iframeUrl } from "./constants";
 class SwapChatSdk {
-  constructor(content, container) {
+  constructor(content, container, options = {}) {
     if (!isDOM()(content) || !isDOM()(container)) {
       return this;
     }
+    const defaultOp = { height: 600 };
+    this.defaultOptions =
+      Object.prototype.toString.call(options) === "[object Object]"
+        ? Object.assign({}, defaultOp, options)
+        : defaultOp;
     this.status = false;
     this.contenWrapper = content;
     this.button = `<div style="
@@ -41,27 +46,29 @@ class SwapChatSdk {
     });
   }
   creatClient() {
+    const that = this;
     if (this.currentMessageBoxEle) {
       this.container.appendChild(this.currentMessageBoxEle);
       return;
     }
-    let messageBoxEle = createElement(`<div id="web3-housechan-message-box">
+    let messageBoxEle =
+      createElement(`<div id="web3-housechan-message-box" style="min-width:120px;width:100%;width:${this.defaultOptions.width}px;overflow:auto">
             </div>`);
     let homeIconEle = createElement(
-      '<img class="home-icon" src="https://chat.web3messaging.online/assets/icon/newHomeHeaderIcon.svg" alt="">'
+      "<img class='home-icon' src='https://chat.web3messaging.online/assets/icon/newHomeHeaderIcon.svg' alt='' style='width: auto;height: 28px;margin-right: 8px;border-radius: 4px;' >"
     );
     let goHomeIconEle = createElement(
-      '<img class="go-home-icon" src="https://d97ch61yqe5j6.cloudfront.net/frontend/newRefreshIconx3.png" alt="">'
+      "<img class='go-home-icon' src='https://d97ch61yqe5j6.cloudfront.net/frontend/newRefreshIconx3.png' alt='' style='position: absolute;right: 50px;height: 22px;width: auto;z-index: 999;margin-right: 8px;border-radius: 4px; cursor: pointer;' >"
     );
     let slideToggleIconELe = createElement(
-      '<img class="slide-toggle-icon" id="web3-slide-toggle-icon"  src="https://d97ch61yqe5j6.cloudfront.net/frontend/headerDown.png" alt="">'
+      "<img class='slide-toggle-icon' id='web3-slide-toggle-icon'  src='https://d97ch61yqe5j6.cloudfront.net/frontend/headerUp.png' alt='' style='position: absolute;right: 12px;height: 22px;width: auto;z-index: 999;margin-right: 8px;border-radius: 4px;cursor: pointer;'>"
     );
     let messageHeaderEle =
-      createElement(`<div class="twitter-housechan-message-header" >
+      createElement(`<div class="twitter-housechan-message-header" style='min-width:240px;position:relative;width:100%' >
             </div>
         `);
     let messageBodyEle = createElement(
-      "<div class='twitter-housechan-message-body' id ='twitter-housechan-message-body' style='overflow:hidden;transition: max-height .25s;max-height:666px'></div>"
+      "<div class='twitter-housechan-message-body' id ='twitter-housechan-message-body' style='overflow:hidden;transition: max-height .25s;max-height:1000px'></div>"
     );
     addEvent(messageHeaderEle, "click", function () {
       const slideToggleIconElement = getElementById("web3-slide-toggle-icon");
@@ -77,7 +84,7 @@ class SwapChatSdk {
           "src",
           "https://d97ch61yqe5j6.cloudfront.net/frontend/headerUp.png"
         );
-        messageBodyEle.style.maxHeight = "666px";
+        messageBodyEle.style.maxHeight = "1000px";
       }
     });
     addEvent(goHomeIconEle, "click", function (e) {
@@ -85,7 +92,7 @@ class SwapChatSdk {
       const IframeDomWrapper = getElementById("twitter-housechan-message-body");
       if (IframeDomWrapper) {
         IframeDomWrapper.innerHTML = "";
-        IframeDomWrapper.innerHTML = `<iframe class="twitter-housechan-message-header-iframe" style='width: 100%; height: 600px; border: 0;' src="https://chat.web3messaging.online"></iframe>`;
+        IframeDomWrapper.innerHTML = `<iframe class="twitter-housechan-message-header-iframe" style='width: 100%; height: ${that.defaultOptions.height}px; border: 0;' src="https://chat.web3messaging.online"></iframe>`;
       }
     });
     messageHeaderEle.appendChild(homeIconEle);
@@ -94,7 +101,7 @@ class SwapChatSdk {
     messageBoxEle.appendChild(messageHeaderEle);
     messageBodyEle.innerHTML = `<iframe
         class="twitter-housechan-message-header-iframe"
-        style="width: 100%; height: 600px; border: 0;"
+        style="width: 100%; height: ${this.defaultOptions.height}px; border: 0;"
         src="https://chat.web3messaging.online"
       ></iframe>`;
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/index.ts",
   mode: "production",
+  //   watch: true,
+  watchOptions: {
+    // 默认为空，不监听的文件或文件夹，支持正则匹配如：ignored: "files/**/*.js"
+    // 如果是多个，可以使用数组：ignored: ['files/**/*.js', 'node_modules']
+    ignored: /node_modules/,
+    // 监听到变化后会等 300ms 再去执行，默认 300ms
+    aggregateTimeout: 300,
+    // 判断文件是否发生变化是通过不停询问系统指定文件有没有变化实现的，默认每秒问 1000 次
+    poll: 1000,
+  },
   output: {
     path: path.resolve(__dirname, "lib"),
     filename: "index.js",
